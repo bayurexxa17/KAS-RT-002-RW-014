693353093f87af9bed44c4d9e98a01c103abadf6
bayurexxa17 <bayurexxa17@gmail.com>
2026-09-08 17:45:42 +0700

    Delete get_nik.py

--- get_nik.py
+++
@@ -1,9 +0,0 @@
-from backend.database import SessionLocal
-from backend.models import Warga
-
-try:
-    db = SessionLocal()
-    warga = db.query(Warga).first()
-    print(f"Name: {warga.nama} | NIK: {warga.nik}")
-except Exception as e:
-    print(f"Error: {e}")
